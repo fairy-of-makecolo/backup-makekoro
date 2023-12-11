b3f6e0bf64ef3ec295e29541a1bb0d16fcc5a271
nitoriYTEk <>
2023-12-11 11:34:13 +0900

    改行付与

--- package/fetch_content.js
+++ package/fetch_content.js
@@ -43,8 +43,7 @@ for (var i = 0; i < sheetNames.length; ++i) {
       return line
   })
 
-  const content = expandedRange.map(line => line.map(str => str.replaceAll('\n', '\\n')).join('\t')).join('\n')
-  // console.log(content)
+  const content = expandedRange.map(line => line.map(str => str.replaceAll('\n', '\\n')).join('\t')).join('\n') + '\n'
   fs.writeFile(sheetDir + "/" + sheetName + ".tsv", content, (err, data) => {
     if(err) console.log(err);
   })
